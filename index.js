cd19cf90a1dcd6d04a389a7e832cd359ecb3919f
Ritesh Biswakarma <119935389+heymrgoodboy@users.noreply.github.com>
2024-06-26 11:09:30 +0530

    learned about increment operators and decrement

---
+++ index.js
@@ -0,0 +1,6 @@
+let a=20;
+let b=10;
+a++; b++;// using the increment operator to  increase the values of a and b by 1
+ console.log(a,b);
+a--; b--;// using the decrement operator to decrease the values of a and b by1
+console.log(a,b);
