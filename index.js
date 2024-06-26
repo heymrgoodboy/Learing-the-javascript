3c1810c78f77e433ab7c02cb847fb26b662b3c2a
Ritesh Biswakarma <119935389+heymrgoodboy@users.noreply.github.com>
2024-06-26 11:15:52 +0530

    used arithemetic and uinary operators

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let a=20;
 let b=10;
-/*
+
 a++; b++;// using the increment operator to  increase the values of a and b by 1
  console.log(a,b);
 a--; b--;// using the decrement operator to decrease the values of a and b by1
 console.log(a,b);
-*/
+
 // using the arithemetic operators
 
 console.log("Sum of the a and b is " +  (a+b));
