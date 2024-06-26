29fc99a1dcb5c6e69b27bd0e7962a5450575e078
Ritesh Biswakarma <119935389+heymrgoodboy@users.noreply.github.com>
2024-06-26 11:14:24 +0530

    used some arithemetic operatora in javascript

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 let a=20;
 let b=10;
+/*
 a++; b++;// using the increment operator to  increase the values of a and b by 1
  console.log(a,b);
 a--; b--;// using the decrement operator to decrease the values of a and b by1
 console.log(a,b);
+*/
+// using the arithemetic operators
+
+console.log("Sum of the a and b is " +  (a+b));
+
+console.log("Difference of the a and b is " +  (a-b));
+
+console.log("Product of the a and b is " +  (a*b));
+
+console.log("Division of the a and b is " +  (a/b));
+
+console.log("Modulus of the a and b is " +  (a%b));
